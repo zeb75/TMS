22c224fff338a24a6cd3918afa534d3d47ad8a17
Fredrik Lundqvist <zeb75@hotmail.com>
2019-01-11 16:21:13 +0100

    UserReport added

--- TMS/src/main/java/com/tms/controller/TimeLogController.java
+++ TMS/src/main/java/com/tms/controller/TimeLogController.java
@@ -1,13 +1,11 @@
 package com.tms.controller;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tms.model.TimeLog;
--- TMS/src/main/java/com/tms/controller/TimeReportController.java
+++ TMS/src/main/java/com/tms/controller/TimeReportController.java
@@ -1,18 +1,42 @@
 package com.tms.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tms.model.TimeLog;
+import com.tms.repository.TimeLogRepository;
 
 @Controller
 public class TimeReportController {
 
-	
-	@GetMapping("/userReport")
-	public String timeReport(Model model, String userId )
-	{
-		model.addAttribute("userId", userId);
-		
-		return "userReport";
-	}
+    @Autowired
+    private TimeLogRepository timeLogRepository;
+
+    @GetMapping("/userTimeReport")
+    public String StartTimeReport(Model model,@RequestParam Integer userId){
+
+        model.addAttribute("userId", userId);
+        System.out.println("####### GET REPOSRT " + userId);
+        return "userTimeReport";
+    }
+
+    @PostMapping("/userTimeReport")
+    public String timeReport(Model model,@RequestParam Integer userId,@RequestParam String startTime,@RequestParam String stopTime) {
+        LocalDateTime startT = LocalDateTime.parse(startTime+"T00:00:00");
+        LocalDateTime stopT = LocalDateTime.parse(stopTime+"T00:00:00");
+        List<TimeLog> reportTimes = timeLogRepository.findByUserIdAndStartTimeBetween(userId,startT,stopT);
+        model.addAttribute("reportTimes", reportTimes);
+        model.addAttribute("userId",userId);
+
+        System.out.println("######### POST REPORT" + userId);
+
+        return "userTimeReport";
+    }
 }
